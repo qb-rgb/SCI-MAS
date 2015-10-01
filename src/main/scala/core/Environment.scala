65a67e7e6012d414fff38a540e7664c48d51848b
qw0te <baert.quentin@gmail.com>
2015-10-01 20:38:29 +0200

    Ajoute des méthodes à la classe Environment

--- src/main/scala/core/Environment.scala
+++ src/main/scala/core/Environment.scala
@@ -8,10 +8,10 @@ package core
   *
   * @author Quentin Baert
   */
-abstract class Environment(width: Int, height: Int) {
+abstract class Environment(val width: Int, val height: Int) {
 
-  /** Cells of the environment. */
-  val cells = Array.ofDim[Cell](width, height)
+
+  protected val cells = Array.ofDim[Cell](this.width, this.height)
 
   /** Determine if a cell of the environment is empty or not.
     *
@@ -21,6 +21,14 @@ abstract class Environment(width: Int, height: Int) {
     */
   def isEmpty(x: Int, y: Int): Boolean = this.cells(x)(y) == None
 
+  /** Determine if a cell of the environment is full or not
+    *
+    * @param x abscissa of the cell
+    * @param y ordinate of the cell
+    * @return true if the cell is full, false otherwise
+    */
+  def isFull(x: Int, y: Int): Boolean = !this.isEmpty(x, y)
+
   /** Determine if a cell is under the environment or not.
     *
     * @param x abscissa of the cell
@@ -29,5 +37,31 @@ abstract class Environment(width: Int, height: Int) {
     */
   def isUnder(x: Int, y: Int): Boolean =
     (0 <= x && x < this.width) && (0 <= y && y < this.height)
+
+  /** Determine if a cell is out of the environment or not.
+    *
+    * @param x abscissa of the cell
+    * @param y ordinate of the cell
+    * @return true if the cell is out of the environment, false otherwise
+    */
+  def isOut(x: Int, y: Int): Boolean = !this.isUnder(x, y)
+
+  /** Add an agent in the environment.
+    *
+    * @param a agent to add in the environment
+    */
+  def addAgent(a: Agent): Unit =
+    this.cells(a.posX)(a.posY) = Some(a)
+
+  /** Empty a cell.
+    *
+    * @param x abscissa of the cell
+    * @param y ordinate of the cell
+    */
+  def emptyCell(x: Int, y: Int): Unit =
+    this.cells(x)(y) = None
+
+  def getAgentAt(x: Int, y: Int): Agent =
+    this.cells(x)(y).get
 
 }
