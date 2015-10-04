5f5757abb3546e528220105a7aad3f82d4509bbd
qw0te <baert.quentin@gmail.com>
2015-10-04 22:18:58 +0200

    Ajoute de la documentation

--- src/main/scala/core/MAS.scala
+++ src/main/scala/core/MAS.scala
@@ -2,10 +2,22 @@ package core
 
 import scala.util.Random
 
+/** Represent a Multi-agent system.
+  * The MAS object give the speak at all the agents in its environment.
+  *
+  * @constructor create a new Multi-agent system.
+  * @param environment environment of the MAS
+  *
+  * @author Quentin Baert
+  */
 class MAS(val environment: Environment) {
 
+  /** Agents in the environment. */
   def agents: List[Agent] = this.environment.getAgents
 
+  /** Run one turn of the MAS.
+    * In one turn, each agent of the environment speak one time.
+    */
   def runOneTurn: Unit = {
     val orderedAgents = Random shuffle this.agents
 
