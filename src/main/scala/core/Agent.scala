d0f631f47c10ca711b8f72bbffa7eee049b327f1
qw0te <baert.quentin@gmail.com>
2015-10-04 11:37:04 +0200

    Ajoute de la documentation

--- src/main/scala/core/Agent.scala
+++ src/main/scala/core/Agent.scala
@@ -6,12 +6,16 @@ package core
   */
 trait Agent {
 
+  /** Environment in which the agent evolve. */
   val environment: Environment
 
+  /** Abscissa of the agent in its environment. */
   def posX: Int
 
+  /** Ordinate of the agent in its environment. */
   def posY: Int
 
+  /** Make the agent take a decision. */
   def decide: Unit
 
 }
