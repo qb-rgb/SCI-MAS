415680e520a993ace1ecb8e72d613fe16f1dd55a
qw0te <baert.quentin@gmail.com>
2015-10-04 16:38:18 +0200

    Ajoute une classe WatorEnvironment
    Cette classe correspond à l'environnement pour une simulation
    proie-prédateur

---
+++ src/main/scala/wator/WatorEnvironment.scala
@@ -0,0 +1,36 @@
+package wator
+
+import core.Environment
+
+/** Environment for a prey-predator simulation.
+  *
+  * @constructor create a new environment for a prey-predator simulation.
+  * @param width width of the environment
+  * @param height height of the environment
+  *
+  * @author Quentin Baert
+  */
+class WatorEnvironment(
+  override val width: Int,
+  override val height: Int
+) extends Environment(width, height) {
+
+  // Fishes and their positions
+  private var fishes: Map[(Int, Int), Fish] = Map()
+
+  /** @see core.Environment.getAgentAt() */
+  override def getAgentAt(x: Int, y: Int): Fish = this.fishes(x, y)
+
+  /** @see core.Environment.emptyCell() */
+  override def emptyCell(x: Int, y: Int): Unit = {
+    this.fishes = this.fishes - ((x, y))
+    super.emptyCell(x, y)
+  }
+
+  /** @see core.Environment.addAgent() */
+  def addAgent(f: Fish): Unit = {
+    this.fishes = this.fishes + ((f.posX, f.posY) -> f)
+    super.addAgent(f)
+  }
+
+}
