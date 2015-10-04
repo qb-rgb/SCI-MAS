b758e5940ce5a2bfbc294c3f9e99de130c973f4b
qw0te <baert.quentin@gmail.com>
2015-10-04 18:25:11 +0200

    Ajoute un objet WatorConfig
    Cet objet regroupe toutes les valeurs qui servent à la configuration
    d'une simulation proie-prédateur.

---
+++ src/main/scala/wator/WatorConfig.scala
@@ -0,0 +1,18 @@
+package wator
+
+object WatorConfig {
+
+  // ---------- TUNAS ---------- //
+
+  /** Breeding age of the tunas. */
+  val tunaBreedingAge: Int = 2
+
+  // ---------- SHARKS ---------- //
+
+  /** Breeding age of the sharks. */
+  val sharkBreedingAge: Int = 10
+
+  /** Starvation time of the sharks. */
+  val sharkStarvationTime: Int = 3
+
+}
