81b34020bd24bdfb0194ca680ddfce3d900a75d3
qw0te <baert.quentin@gmail.com>
2015-10-04 15:01:36 +0200

    Ajoute un main pour les chambre à particules

---
+++ src/main/scala/particles/ParticlesRoom.scala
@@ -0,0 +1,78 @@
+package particles
+
+import core.MAS
+
+/** Main program.
+  *
+  * @author Quentin Baert
+  */
+object ParticlesRoom {
+
+  type OptionMap = Map[String, String]
+
+  // Error log
+  private val errorLog = "ERROR"
+
+  // Print the usage of the program
+  private def printUsage: Unit = {
+    println("java -jar particleRoom -width [width] -height [height] " +
+            "-number [number of particles] -size [size of the particles] " +
+            "-sleep [time to sleep between two turns]")
+    sys.exit(1)
+  }
+
+  // Catch the option of the program
+  private def catchOptions(args: Array[String]): OptionMap = {
+    def nextOption(map: OptionMap, args: List[String]): OptionMap = args match {
+      // -width option
+      case "-width" :: value :: tail => nextOption(map ++ Map("-width" -> value), tail)
+
+      // -height option
+      case "-height" :: value :: tail => nextOption(map ++ Map("-height" -> value), tail)
+
+      // -number option
+      case "-number" :: value :: tail => nextOption(map ++ Map("-number" -> value), tail)
+
+      // -size option
+      case "-size" :: value :: tail => nextOption(map ++ Map("-size" -> value), tail)
+
+      // -sleep option
+      case "-sleep" :: value :: tail => nextOption(map ++ Map("-sleep" -> value), tail)
+
+      // Base case
+      case Nil => map
+
+      // Error case
+      case _ => Map("unknownOption" -> this.errorLog)
+    }
+
+    val argsList = args.toList
+
+    nextOption(Map(), argsList)
+  }
+
+  // Check if the options are ok
+  private def checkOptions(map: OptionMap): Boolean =
+    !map.isEmpty && !(map.values.toList contains this.errorLog)
+
+  // Main
+  def main(args: Array[String]): Unit = {
+    val options = this.catchOptions(args)
+
+    if (this.checkOptions(options)) {
+      val width = options("-width").toInt
+      val height = options("-height").toInt
+      val number = options("-number").toInt
+      val size = options("-size").toInt
+      val sleep = options("-sleep").toInt
+
+      val env = new ParticlesEnvironment(width, height)
+      val particles = new ParticlesGenerator(env).generate(number)
+      val mas = new MAS(env, particles)
+
+      new ParticlesRoomUI(mas, size, sleep).run
+    } else
+      this.printUsage
+  }
+
+}
