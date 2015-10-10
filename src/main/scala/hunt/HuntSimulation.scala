e195de524446ddef501079a1e2f83c6bbfaf36b9
qw0te <baert.quentin@gmail.com>
2015-10-10 17:23:08 +0200

    Ajoute le main de la simulation de chasse

---
+++ src/main/scala/hunt/HuntSimulation.scala
@@ -0,0 +1,65 @@
+package hunt
+
+/** Main program.
+  *
+  * @author Quentin Baert
+  */
+object HuntSimulation {
+
+  type OptionMap = Map[String, String]
+
+  // Error log
+  private val errorLog = "ERROR"
+
+  // Print the usage of the program
+  private def printUsage: Unit = {
+    println("TO DO")
+    sys.exit(1)
+  }
+
+  // Catch the option of the program
+  private def catchOptions(args: Array[String]): OptionMap = {
+    def nextOption(map: OptionMap, args: List[String]): OptionMap = args match {
+      // model option
+      case "-model" :: value :: tail => nextOption(map ++ Map("-model" -> value), tail)
+
+      // -size option
+      case "-size" :: value :: tail => nextOption(map ++ Map("-size" -> value), tail)
+
+      // -sleep option
+      case "-sleep" :: value :: tail => nextOption(map ++ Map("-sleep" -> value), tail)
+
+      // Base case
+      case Nil => map
+
+      // Error case
+      case _ => Map("unknownOption" -> this.errorLog)
+    }
+
+    val argsList = args.toList
+
+    nextOption(Map(), argsList)
+  }
+
+  // Check if the options are ok
+  private def checkOptions(map: OptionMap): Boolean =
+    !map.isEmpty && !(map.values.toList contains this.errorLog)
+
+  // Main
+  def main(args: Array[String]): Unit = {
+    val options = this.catchOptions(args)
+
+    if (this.checkOptions(options)) {
+      val size = options("-size").toInt
+      val sleep = options("-size").toInt
+      val model = options("-model")
+
+      val env = ModelBuilder.buildEnvFromModel(model)
+      val mas = new HuntMAS(env)
+
+      new HuntUI(mas, size, sleep).run
+    } else
+      this.printUsage
+  }
+
+}
